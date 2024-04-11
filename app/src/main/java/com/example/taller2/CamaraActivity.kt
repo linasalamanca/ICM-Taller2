b92ea77528187b9c07318c13f301b46c526272e6
SailorMusic05 <141599762+SailorMusic05@users.noreply.github.com>
2024-04-11 14:48:59 -0500

    Camara XML

--- app/src/main/java/com/example/taller2/CamaraActivity.kt
+++ app/src/main/java/com/example/taller2/CamaraActivity.kt
@@ -1,6 +1,8 @@
 package com.example.taller2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,12 +11,10 @@ import androidx.core.view.WindowInsetsCompat
 class CamaraActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_camara)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val btnGaleria = findViewById<Button>(R.id.btnGaleria)
+        val btnCamara = findViewById<Button>(R.id.btnCamara)
+        val imagen = findViewById<ImageView>(R.id.imagenCargada)
     }
 }
